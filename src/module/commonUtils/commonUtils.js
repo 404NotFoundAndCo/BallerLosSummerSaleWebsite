46067ae8ecfbabc25dd2461e26c6b3cda4daca2b
Sam <samuel.nakata@gmail.com>
2024-11-08 11:37:00 +0100

    Added Iframe load method and active button state method

--- src/module/commonUtils/commonUtils.js
+++ src/module/commonUtils/commonUtils.js
@@ -12,3 +12,20 @@ toggleModeButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
   document.body.classList.toggle('light-mode');
 });
+
+//Javascript for changing iframe content
+function loadIframeContent(page) {
+  document.getElementById('contentFrame').src = page;
+}
+
+/* JavaScript for button active state */
+function setActiveButton() {
+  document.querySelectorAll('nav a, #toggle-mode').forEach(function(button) {
+    button.addEventListener('click', function() {
+      document.querySelectorAll('nav a, #toggle-mode').forEach(function(btn) {
+        btn.classList.remove('active');
+      });
+      this.classList.add('active');
+    });
+  });
+}
