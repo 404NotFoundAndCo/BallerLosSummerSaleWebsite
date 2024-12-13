36ba03cba57c82dcd0cfb69bed5438391c8a5d0a
Eitan <eitan.mihaescu@stud.hs-hannover.de>
2024-12-13 10:47:18 +0100

    Carousel update: clicking on buttons and images works!

--- src/module/product/product.js
+++ src/module/product/product.js
@@ -1,3 +1,11 @@
+const images = [
+    './Images/image1.jpg',
+    './Images/image2.jpg',
+    './Images/image3.jpg'
+]
+
+let currentIndex = 0;
+
 function changeImage(src) {
     const mainImage = document.getElementById('current-image');
     const previews = document.querySelectorAll('.preview');
@@ -13,7 +21,14 @@ function changeImage(src) {
     }
 }
 
-// Automatically select the first image on load
-window.onload = function() {
-    changeImage('FingerabdruckRecherche.png');
-};
+function toImage(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1; // Loop back to last image
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0; // Loop back to first image
+    }
+
+    changeImage(images[currentIndex])
+}
+
